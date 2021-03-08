9ead19a3cb771913f2fd1736caed46029c2932d1
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-08 16:22:54 +0100

    Spaceship Model & Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Spaceship.create!(
+  name: "Battlestar Galactica",
+  capacity: rand(0..50),
+  speciality: "faster-than-light"
+  )
+Spaceship.create!(
+  name: "Discovery",
+  capacity: rand(0..50),
+  speciality: "avoids enemy attacks"
+  )
+Spaceship.create!(
+  name: "Eagle Transporter",
+  capacity: rand(0..50),
+  speciality: "nice interior design"
+  )
+Spaceship.create!(
+  name: "USS Sulaco",
+  capacity: rrand(0..50),
+  speciality: "relax pur with own space garden"
+  )
+Spaceship.create!(
+  name: "The Millennium Falcon",
+  capacity: rand(0..50),
+  speciality: "a lot of entertainment on board"
+  )
