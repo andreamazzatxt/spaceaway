e4dc63daa12c6b05848634d9b86c742254413536
stefano marchetti <stefano.marchetti20@gmail.com>
2021-03-08 17:43:11 +0100

    delete captain row from model

--- db/migrate/20210308164021_create_trips.rb
+++ db/migrate/20210308164021_create_trips.rb
@@ -2,9 +2,8 @@ class CreateTrips < ActiveRecord::Migration[6.1]
   def change
     create_table :trips do |t|
       t.string :name
-      t.references :spaceships, null: false, foreign_key: true
-      t.references :planets, null: false, foreign_key: true
-      t.references :captains, null: false, foreign_key: true
+      t.references :spaceship, null: false, foreign_key: true
+      t.references :planet, null: false, foreign_key: true
       t.integer :price
       t.date :departure_date
       t.date :arrival_date
