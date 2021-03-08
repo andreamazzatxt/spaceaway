185683310cb0bb1ed0d81df339a01847a83a1333
andreamazzatxt <andreamazza89@gmail.com>
2021-03-08 18:41:43 +0100

    fix migration priorities

--- db/migrate/20210308171443_create_reviews.rb
+++ db/migrate/20210308171443_create_reviews.rb
@@ -3,7 +3,6 @@ class CreateReviews < ActiveRecord::Migration[6.1]
     create_table :reviews do |t|
       t.string :type
       t.references :user, null: false, foreign_key: true
-      t.references :booking, null: false, foreign_key: true
       t.integer :rating
       t.text :content
 
