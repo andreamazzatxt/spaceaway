a81911547d61fecd8770cdc0c59c66c636679b69
Lisa Fellinger <fellingerlisa@hotmail.com>
2021-03-10 14:35:23 +0100

    add captain_id to trips, create trip-index-page & create review on showpage

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :spaceship
   belongs_to :planet
+  belongs_to :captain, class_name: :User, foreign_key: :captain_id
   has_many :bookings
   has_many :users, through: :bookings
   has_many :reviews, through: :bookings
